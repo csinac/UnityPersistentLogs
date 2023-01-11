0e40a1223fec8b0a1cab799589a7e80887807ed1
rectdev <cemsinacetin@gmail.com>
2023-01-11 08:42:37 +0000

    Windows behaviour implemented

--- Assets/PersistentLogs/Scripts/ProcessListener.cs
+++ Assets/PersistentLogs/Scripts/ProcessListener.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
 
 namespace SinaC.PersistentLogs
 {
@@ -9,16 +11,63 @@ namespace SinaC.PersistentLogs
         [SerializeField] private Text output;
         
         private int pid;
-        private string processName;
+        private string logLocation;
+        private string scriptPath;
+        private string outputLocation;
         
         void Start() {
+#if UNITY_STANDALONE_WIN
+            scriptPath = Path.Combine(Application.streamingAssetsPath, "processListener.bat");
+            outputLocation = Path.Combine(Directory.GetParent(Application.dataPath).ToString(), "Logs");
+            logLocation = Application.persistentDataPath;
             pid = Process.GetCurrentProcess().Id;
-            processName = Process.GetCurrentProcess().ProcessName;
+
+            scriptPath = scriptPath.Replace("/", "\\");
+            outputLocation = outputLocation.Replace("/", "\\");
+            logLocation = logLocation.Replace("/", "\\");
+            
+            Debug.Log(logLocation);
+            Debug.Log(scriptPath);
+            Debug.Log(outputLocation);
+            Debug.Log(pid);
+#else
+            Debug.LogWarning("Works only on Windows yet");
+            Destroy(this);
+            return;
+#endif
+
+            if (!File.Exists(scriptPath)) {
+                Destroy(this);
+                return;
+            }
             
             output.text = string.Join("\n",new[] {
                 pid.ToString(),
-                processName
+                logLocation,
+                outputLocation
             });
+
+            if (!Directory.Exists(outputLocation)) {
+                Directory.CreateDirectory(outputLocation);
+            }
+
+            if (Application.isEditor)
+                return;
+            
+#if UNITY_STANDALONE_WIN
+            RunBatchScript();
+#endif
         }
+        
+#if UNITY_STANDALONE_WIN
+        private void RunBatchScript() {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = scriptPath;
+            startInfo.Arguments = $"{pid} \"{logLocation}\" Player log \"{outputLocation}\"";
+            Process.Start(startInfo);
+
+            output.text = $"{output.text}\n{startInfo.Arguments}";
+        }
+#endif
     }
 }
